90123894307f2e325d6542410f1933febdc027b1
sarapappila <sara.pappila@metropolia.fi>
2024-09-17 10:50:19 +0300

    tehty

--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
 
-public class TemperatureConverterTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TemperatureConverterTest {
+
+
+    private TemperatureConverter converter;
+
+    @BeforeEach
+    public void setUp() {
+        converter = new TemperatureConverter();
+    }
+
+    @Test
+    void fahrenheitToCelsius() {
+        Assertions.assertEquals(0, converter.fahrenheitToCelsius(32), 0.01);
+        Assertions.assertEquals(100, converter.fahrenheitToCelsius(212), 0.01);
+        Assertions.assertEquals(-40, converter.fahrenheitToCelsius(-40), 0.01);
+    }
+
+    @Test
+    public void testCelsiusToFahrenheit() {
+        Assertions.assertEquals(32, converter.celsiusToFahrenheit(0), 0.01);
+        Assertions.assertEquals(212, converter.celsiusToFahrenheit(100), 0.01);
+        Assertions.assertEquals(-40, converter.celsiusToFahrenheit(-40), 0.01);
+    }
+
+    @Test
+    public void testIsExtremeTemperature() {
+        Assertions.assertTrue(converter.isExtremeTemperature(-50));
+        Assertions.assertFalse(converter.isExtremeTemperature(20));
+        Assertions.assertTrue(converter.isExtremeTemperature(60));
+    }
+
+    @Test
+    public void testKelvinToCel() {
+        Assertions.assertEquals(0.0, TemperatureConverter.kelvinToCel(273.15F), 0.001);
+    }
+
+
 }
